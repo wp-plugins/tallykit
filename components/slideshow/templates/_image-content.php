cd4d798de43e74b2ca168262c6659f20a2b84ac7
tallythemes <tallythemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-02 16:54:37 +0000

    add missing files
    git-svn-id: https://plugins.svn.wordpress.org/tallykit/trunk@942301 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- components/slideshow/templates/_image-content.php
+++
@@ -1,4 +0,0 @@
-<div class="tk_slideshow_image_content_warp">
-	<?php include(tallykit_slideshow_template_path('dri').'content/content-image.php'); ?>
-	<?php include(tallykit_slideshow_template_path('dri').'content/content-content.php'); ?>
-</div>
--- components/slideshow/templates/_video-content.php
+++
@@ -1,4 +0,0 @@
-<div class="tk_slideshow_video_content_warp">
-    <?php include(tallykit_slideshow_template_path('dri').'content/content-video.php'); ?>
-    <?php include(tallykit_slideshow_template_path('dri').'content/content-content.php'); ?>
-</div>
