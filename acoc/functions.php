4389f1256bd35af07c2646b545e22b985655b26c
tallythemes <tallythemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-12 04:48:15 +0000

    tagged 1.6
    git-svn-id: https://plugins.svn.wordpress.org/tallykit/tags/1.6@947287 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ acoc/functions.php
@@ -0,0 +1,164 @@
+<?php
+/* Resize image
+-------------------------------------------------*/
+if(!function_exists('acoc_image_size')):
+function acoc_image_size($url, $width = '', $height = '', $crop = true, $align = '', $retina = ACOC_IMAGE_RETINA_SUPPORT){
+	global $wpdb;
+	
+    $query = "SELECT ID FROM {$wpdb->posts} WHERE guid='$url'";
+    $id = $wpdb->get_var($query);
+	
+	$url = ( $url == "" ) ? 'http://placehold.it/'.$width.'x'.$height.'' : $url;
+	
+	if(function_exists('mr_image_resize')){
+		if($id == false){
+			return $url;
+		}else{
+			return mr_image_resize($url, $width, $height, $crop, $align, $retina);
+		}
+	}else{
+		return $url;
+	}
+}
+endif;
+
+
+/**
+ * Helper function to return encoded strings
+ *
+ * @return    string
+ *
+ * @access    public
+ * @since     1.0
+ */
+function acoc_encode( $value ) {
+
+  $func = 'base64' . '_encode';
+  return $func( $value );
+  
+}
+
+/**
+ * Helper function to return decoded strings
+ *
+ * @return    string
+ *
+ * @access    public
+ * @since    1.0
+ */
+function acoc_decode( $value ) {
+
+  $func = 'base64' . '_decode';
+  return $func( $value );
+  
+}
+
+/* Output Content Nav
+-------------------------------------------------*/
+if(!function_exists('acoc_paginate')):
+function acoc_paginate($query = ''){
+	$output = NULL;
+	if ($query->max_num_pages > 1) {
+		$output .= '<div class="pagenav-acoc">';
+			$big = 999999999; // need an unlikely integer		
+			$output .= paginate_links( array(
+				'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+				'format' => '?paged=%#%',
+				'current' => max( 1, get_query_var('paged') ),
+				'total' => $query->max_num_pages
+			));
+		$output .= '</div>';
+	}
+	return $output;
+}
+endif;
+
+
+/* Display  post_thumbnail
+-------------------------------------------------*/
+if(!function_exists('acoc_post_thumbnail')):
+function acoc_post_thumbnail($args = array()){
+	$default = array(			
+		'id' => get_the_ID(),		
+		'w' => '100',
+		'h' => '100',
+		'crop' => true,
+		'placeholder' => NULL,
+	);
+	$args = array_merge($default, $args);
+	
+	$output = acoc_image_size('', $args['w'], $args['h'], $args['crop']);
+	
+	if ( function_exists( 'has_post_thumbnail' ) && has_post_thumbnail( $args['id'] ) ){
+		$thumb_id = get_post_thumbnail_id($args['id']);
+		$thumb_url = wp_get_attachment_image_src($thumb_id, 'full', true);
+		$output = acoc_image_size($thumb_url[0], $args['w'], $args['h'], $args['crop']);
+	}elseif($args['placeholder'] != ''){
+		$output = $args['placeholder'];
+	}
+	
+	return $output;
+}
+endif;
+
+
+
+/* Category name of the post
+--------------------------------------------------------*/
+function acoc_post_taxonomys_name($post_id, $taxonomy, $divider = ", "){
+	$terms = get_the_terms( $post_id, $taxonomy );
+						
+	if ( $terms && ! is_wp_error( $terms ) ) : 
+		$draught_links = array();
+		foreach ( $terms as $term ) {
+			$draught_links[] = $term->name;
+		}					
+		$on_draught = join( $divider, $draught_links );
+		return $on_draught;
+	else:
+		return '&nbsp;';
+	endif; 
+}
+
+/* Category name of the post
+--------------------------------------------------------*/
+function acoc_post_taxonomys_link($post_id, $taxonomy, $divider = ", "){
+	$terms = get_the_terms( $post_id, $taxonomy );
+						
+	if ( $terms && ! is_wp_error( $terms ) ) : 
+		$draught_links = array();
+		foreach ( $terms as $term ) {
+			$draught_links[] = '<a href="'.get_term_link($term->slug, $taxonomy).'">'.$term->name.'</a>';
+		}					
+		$on_draught = join( $divider, $draught_links );
+		return $on_draught;
+	else:
+		return '&nbsp;';
+	endif; 
+}
+
+
+/* ACOC post excerpt
+--------------------------------------------------------*/
+function acoc_max_charlength($charlength, $text = NULL) {
+	if ($text) {
+		$excerpt = $text;
+	} else {
+		$excerpt = get_the_excerpt();
+	}
+
+	$charlength++;
+	if (strlen($excerpt)>$charlength) {
+		$subex   = substr($excerpt,0,$charlength-5);
+        $exwords = explode(" ",$subex);
+		$excut   = -(strlen($exwords[count($exwords)-1]));
+		if ($excut<0) {
+			return substr($subex,0,$excut);
+		} else {
+			return $subex;
+		}
+		return '..';
+	} else {
+		return $excerpt;
+	}
+}
---
+++ components/slideshow/templates/slideshow.php
@@ -0,0 +1,39 @@
+<?php 
+$slider_items = get_post_meta($id, 'tallykit_slideshow_slider_items', true ); 
+$flexslider2 = new acoc_flexslider2_html(array(
+	'animation'        => get_post_meta($id, 'tallykit_slideshow_animation', true ),
+	'direction'        => get_post_meta($id, 'tallykit_slideshow_direction', true ),
+	'slideshowSpeed'   => get_post_meta($id, 'tallykit_slideshow_slideshowSpeed', true ),
+	'animationSpeed'   => get_post_meta($id, 'tallykit_slideshow_animationSpeed', true ),
+	'controlNav'       => get_post_meta($id, 'tallykit_slideshow_controlNav', true ),
+	'directionNav'     => get_post_meta($id, 'tallykit_slideshow_directionNav', true ),
+	'prevText' => '',
+	'nextText' => '',
+));
+
+//print_r($slider_items);
+
+include(tallykit_slideshow_template_path('dri').'slideshow-style.php');
+?>
+<?php if(is_array($slider_items)): $i = 1; ?>    
+    <div class="tallykit_slideshow">
+        <div class="tallykit_slideshow_inner">
+            <?php 
+			$i = 1;
+            $flexslider2->start();
+                foreach($slider_items as $slider_item):
+                    $flexslider2->in_loop_start();
+                        echo '<div class="tallykit_slideshow_item '.$slider_item['type'].' color_mood_'.$slider_item['color_mood'].'" id="tallykit_slideshow_item_'.$i.'">';
+                            echo '<div class="tallykit_slideshow_item_inner">';
+                                include(tallykit_slideshow_template_path('dri').'_'.$slider_item['type'].'.php');
+                            echo '</div>';
+                        echo '</div>';
+                    $flexslider2->in_loop_end();
+                $i++; endforeach; 
+            $flexslider2->end();
+            ?>
+        </div>
+    </div>
+<?php else: ?>
+	No slider Item's found
+<?php endif; ?>
